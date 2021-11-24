f6d8111c504ed775661ee07cadc2e5739946eb34
Palputa <56628009+Palputa@users.noreply.github.com>
2021-11-24 20:04:11 +0100

    nazwy
    zmiany

--- gallery3.sql
+++ gallery3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 21 Lis 2021, 16:14
+-- Czas generowania: 17 Lis 2021, 20:44
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.9
 
@@ -26,9 +26,18 @@ SET time_zone = "+00:00";
 --
 -- Struktura tabeli dla tabeli `album`
 --
+CREATE TABLE `user` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) COLLATE utf8_polish_ci NOT NULL,
+  `password` text COLLATE utf8_polish_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `last_login` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+   PRIMARY KEY(`id`),
+   UNIQUE KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 CREATE TABLE `album` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(10) COLLATE utf8_polish_ci NOT NULL,
   `photo_amount` bigint(20) UNSIGNED NOT NULL,
   `id_user` bigint(20) UNSIGNED NOT NULL,
@@ -36,8 +45,28 @@ CREATE TABLE `album` (
   `last_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `description` text COLLATE utf8_polish_ci NOT NULL,
   `views` bigint(20) UNSIGNED NOT NULL,
-  `likes` bigint(20) UNSIGNED NOT NULL
+  `likes` bigint(20) UNSIGNED NOT NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY(`id`),
+    FOREIGN KEY(`id_user`) REFERENCES `user`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+CREATE TABLE `photo` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) COLLATE utf8_polish_ci NOT NULL,
+  `id_album` bigint(20) UNSIGNED NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `photo_path` text COLLATE utf8_polish_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `views` bigint(20) UNSIGNED NOT NULL,
+  `likes` bigint(20) UNSIGNED NOT NULL,
+   PRIMARY KEY(`id`),
+   UNIQUE KEY(`id`),
+   FOREIGN KEY(`id_album`) REFERENCES `album`(`id`),
+   FOREIGN KEY(`id_user`) REFERENCES `user`(`id`)
+    
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
 
 -- --------------------------------------------------------
 
@@ -46,183 +75,35 @@ CREATE TABLE `album` (
 --
 
 CREATE TABLE `comment` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `id_photo` bigint(20) UNSIGNED NOT NULL,
   `id_user` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `last_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `likes` bigint(20) UNSIGNED NOT NULL,
-  `content` text COLLATE utf8_polish_ci NOT NULL
+  `content` text COLLATE utf8_polish_ci NOT NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY(`id`),
+    FOREIGN KEY(`id_photo`) REFERENCES `photo`(`id`),
+    FOREIGN KEY(`id_user`) REFERENCES `user`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `photo`
---
-
-CREATE TABLE `photo` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(10) COLLATE utf8_polish_ci NOT NULL,
-  `id_album` bigint(20) UNSIGNED NOT NULL,
-  `id_user` bigint(20) UNSIGNED NOT NULL,
-  `photo_path` text COLLATE utf8_polish_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `views` bigint(20) UNSIGNED NOT NULL,
-  `likes` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `tag`
---
 
 CREATE TABLE `tag` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(10) COLLATE utf8_polish_ci NOT NULL
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) COLLATE utf8_polish_ci NOT NULL,
+   PRIMARY KEY(`id`),
+   UNIQUE KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `tags`
---
 
 CREATE TABLE `tags` (
   `id_photo` bigint(20) UNSIGNED NOT NULL,
-  `id_tag` bigint(20) UNSIGNED NOT NULL
+  `id_tag` bigint(20) UNSIGNED NOT NULL,
+   PRIMARY KEY(`id_photo`,`id_tag`),
+   FOREIGN KEY(`id_photo`) REFERENCES `photo`(`id`),
+   FOREIGN KEY(`id_tag`) REFERENCES `tag`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `user`
---
-
-CREATE TABLE `user` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(10) COLLATE utf8_polish_ci NOT NULL,
-  `password` text COLLATE utf8_polish_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `last_login` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
---
--- Indeksy dla zrzutów tabel
---
-
---
--- Indeksy dla tabeli `album`
---
-ALTER TABLE `album`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indeksy dla tabeli `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `id_photo` (`id_photo`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indeksy dla tabeli `photo`
---
-ALTER TABLE `photo`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `id_album` (`id_album`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indeksy dla tabeli `tag`
---
-ALTER TABLE `tag`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indeksy dla tabeli `tags`
---
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`id_photo`,`id_tag`),
-  ADD KEY `id_tag` (`id_tag`);
-
---
--- Indeksy dla tabeli `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT dla tabeli `album`
---
-ALTER TABLE `album`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `comment`
---
-ALTER TABLE `comment`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `photo`
---
-ALTER TABLE `photo`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `tag`
---
-ALTER TABLE `tag`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `user`
---
-ALTER TABLE `user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `album`
---
-ALTER TABLE `album`
-  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
-
---
--- Ograniczenia dla tabeli `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`id_photo`) REFERENCES `photo` (`id`),
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
-
---
--- Ograniczenia dla tabeli `photo`
---
-ALTER TABLE `photo`
-  ADD CONSTRAINT `photo_ibfk_1` FOREIGN KEY (`id_album`) REFERENCES `album` (`id`),
-  ADD CONSTRAINT `photo_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
-
---
--- Ograniczenia dla tabeli `tags`
---
-ALTER TABLE `tags`
-  ADD CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`id_photo`) REFERENCES `photo` (`id`),
-  ADD CONSTRAINT `tags_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `tag` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
